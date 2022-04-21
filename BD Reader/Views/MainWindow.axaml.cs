a02774bc46bf38025f507859c2830576719449a8
Михаил <hugehug@mail.ru>
2022-04-21 22:15:58 +0700

    Добавьте файлы проекта.

---
+++ BD Reader/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace BD_Reader.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
